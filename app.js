9b61d16e4f33853a61b98dff0abb6ff99e93a7e3
shakeeb <shakeebanwar250@gmail.com>
2020-06-06 22:55:57 +0500

    chap1 almost complete

---
+++ app.js
@@ -0,0 +1,43 @@
+// chap #1
+
+/*1.Write a script to greet your website visitor using JS alert
+box.
+
+alert('Helloworld')
+
+*/
+
+
+
+/* 2.Write a script to display following message on your web page:
+
+alert('Error!Please Enter a Valid Password');
+
+*/
+
+/*
+3. Write a script to display following message on your web
+page: (Hint : Use line break)
+
+alert('Welcome to JsLand..\nHappyCoding')
+
+
+*/
+
+/*4. Write a script to display following messages in sequence:
+
+alert('Welcome to JsLand')
+*/
+
+
+/*5. Generate the following message through browser’s
+developer console:
+
+console.log("Hello...I can run Js through my webbrowser console")
+
+*/
+
+// chap 2
+
+
+
